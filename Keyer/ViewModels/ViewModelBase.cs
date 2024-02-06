c01b8efe1dfe404d6a2ed749e68be9dd829a5f70
vladislavstr <58762058+vladislavstr@users.noreply.github.com>
2024-02-07 01:48:16 +0300

    created UI

---
+++ Keyer/ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using ReactiveUI;
+
+namespace Keyer.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
---
+++ Keyer/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace Keyer.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
