6f3863e037ac2d313277ecf4d718e8db9f556b07
vladislavstr <58762058+vladislavstr@users.noreply.github.com>
2024-02-11 11:06:09 +0300

    created FilesService

---
+++ Keyer/Services/FilesService.cs
@@ -0,0 +1,38 @@
+﻿using System.Threading.Tasks;
+using Avalonia.Controls;
+using Avalonia.Platform.Storage;
+using Keyer.Helpers;
+
+namespace Keyer.Services
+{
+    public class FilesService : IFilesService
+    {
+        private readonly Window _target;
+
+        public FilesService(Window target)
+        {
+            _target = target;
+        }
+
+        public async Task<IStorageFile?> OpenFileAsync()
+        {
+            var files = await _target.StorageProvider.OpenFilePickerAsync(new FilePickerOpenOptions()
+            {
+                Title = "Open PNG File",
+                AllowMultiple = false,
+            });
+
+            return files.Count >= 1 ? files[0] : null;
+        }
+
+        public async Task<IStorageFile?> SaveFileAsync()
+        {
+            return await _target.StorageProvider.SaveFilePickerAsync(new FilePickerSaveOptions()
+            {
+                Title = "Save PNG File",
+                DefaultExtension = "png",
+                ShowOverwritePrompt = true
+            });
+        }
+    }
+}
