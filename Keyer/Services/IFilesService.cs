fe8515f75f4f89f1b28578d98e0a866c8dad0dbf
vladislavstr <58762058+vladislavstr@users.noreply.github.com>
2024-02-11 11:01:10 +0300

    created IFilesService

---
+++ Keyer/Services/IFilesService.cs
@@ -0,0 +1,11 @@
+﻿using Avalonia.Platform.Storage;
+using System.Threading.Tasks;
+
+namespace Keyer.Services
+{
+    public interface IFilesService
+    {
+        public Task<IStorageFile?> OpenFileAsync();
+        public Task<IStorageFile?> SaveFileAsync();
+    }
+}
