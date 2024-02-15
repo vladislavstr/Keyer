117451e742d70c2b00832bcea632f3347057732b
vladislavstr <58762058+vladislavstr@users.noreply.github.com>
2024-02-15 19:50:09 +0300

    created OpenFile

--- Keyer/ViewModels/MainWindowViewModel.cs
+++ Keyer/ViewModels/MainWindowViewModel.cs
@@ -1,9 +1,55 @@
-﻿namespace Keyer.ViewModels
+using Avalonia.Media.Imaging;
+using Avalonia.Platform;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Keyer.Helpers;
+using Keyer.Services;
+using Microsoft.Extensions.DependencyInjection;
+using ReactiveUI;
+using System;
+using System.IO;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Keyer.ViewModels
 {
-    public class MainWindowViewModel : ViewModelBase
+    public partial class MainWindowViewModel : ViewModelBase
     {
-#pragma warning disable CA1822 // Mark members as static
-        public string Greeting => "Welcome to Avalonia!";
-#pragma warning restore CA1822 // Mark members as static
+        public Bitmap? ImageFromBinding { get; } = ImageHelper.LoadFromResource(new("avares://Keyer/Assets/auto-insert-error.png"));
+
+        [ObservableProperty] private Bitmap? _file;
+
+        [RelayCommand]
+        private async Task OpenFile(CancellationToken token)
+        {
+            ErrorMessages?.Clear();
+            try
+            {
+                var filesService = App.Current?.Services?.GetService<IFilesService>();
+                if (filesService is null) throw new NullReferenceException("Missing File Service instance.");
+
+                var file = await filesService.OpenFileAsync();
+                if (file is null) return;
+
+                /// <summary>
+                /// Limited the file size to 1 MB to avoid unnecessary complications.
+                /// </summary>
+                if ((await file.GetBasicPropertiesAsync()).Size <= 1024 * 1024 * 1)
+                {
+                    await using var readStream = await file.OpenReadAsync();
+                    File = new Bitmap(readStream);
+                }
+                else
+                {
+                    throw new Exception("File exceeded 1MB limit.");
+                }
+            }
+            catch (Exception e)
+            {
+                ErrorMessages?.Add(e.Message);
+            }
+        }
     }
 }
--- Keyer/ViewModels/ViewModelBase.cs
+++ Keyer/ViewModels/ViewModelBase.cs
@@ -1,8 +1,17 @@
-﻿using ReactiveUI;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using ReactiveUI;
+using System.Collections.ObjectModel;
 
 namespace Keyer.ViewModels
 {
-    public class ViewModelBase : ReactiveObject
+    public partial class ViewModelBase : ObservableObject
     {
+        protected ViewModelBase()
+        {
+            ErrorMessages = new ObservableCollection<string>();
+        }
+
+        [ObservableProperty]
+        private ObservableCollection<string>? _errorMessages;
     }
 }
