92733f90b29066874161c64c8f961ba3e273fc7d
VladW <VIStrelnikov94@gmail.com>
2024-02-16 19:06:13 +0300

    cropped

--- Keyer/Helpers/ImageHelper.cs
+++ Keyer/Helpers/ImageHelper.cs
@@ -1,19 +1,11 @@
 ﻿using Avalonia.Media.Imaging;
 using Avalonia.Platform;
-using Avalonia.Platform.Storage;
 using System;
 
 namespace Keyer.Helpers
 {
     public static class ImageHelper
     {
-        public static FilePickerFileType ImageAll { get; } = new("All Images")
-        {
-            Patterns = new[] { "*.png", "*.jpg", "*.jpeg", "*.gif", "*.bmp" },
-            AppleUniformTypeIdentifiers = new[] { "public.image" },
-            MimeTypes = new[] { "image/*" }
-        };
-
         public static Bitmap LoadFromResource(Uri resourceUri)
         {
             return new Bitmap(AssetLoader.Open(resourceUri));
--- Keyer/Services/FilesService.cs
+++ Keyer/Services/FilesService.cs
@@ -7,21 +7,19 @@ namespace Keyer.Services
     public class FilesService : IFilesService
     {
         private readonly Window _target;
-        //private readonly FilePickerFileType _fileType;
 
-        public FilesService(Window target)/*, FilePickerFileType fileType)*/
+        public FilesService(Window target)
         {
             _target = target;
-            //_fileType = fileType;
         }
 
         public async Task<IStorageFile?> OpenFileAsync()
         {
+
             var files = await _target.StorageProvider.OpenFilePickerAsync(new FilePickerOpenOptions()
             {
                 Title = "Open PNG File",
                 AllowMultiple = false,
-                //FileTypeFilter = new[] { _fileType, FilePickerFileTypes.TextPlain }
             });
 
             return files.Count >= 1 ? files[0] : null;
